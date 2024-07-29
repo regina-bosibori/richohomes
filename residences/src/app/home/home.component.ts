8fbcd90034056b87f593faa01f5908ade9719803
regina-bosibori <msomegina@gmail.com>
2024-07-29 16:29:13 +0300

    included home intro component

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { HomeIntroComponent } from '../home-intro/home-intro.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent,HomeIntroComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
