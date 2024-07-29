d556ba16c00d77550c15b618e3a942b8139340ca
regina-bosibori <msomegina@gmail.com>
2024-07-29 16:30:05 +0300

    generated home intro component

---
+++ residences/src/app/home-intro/home-intro.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home-intro',
+  standalone: true,
+  imports: [],
+  templateUrl: './home-intro.component.html',
+  styleUrl: './home-intro.component.css'
+})
+export class HomeIntroComponent {
+
+}
