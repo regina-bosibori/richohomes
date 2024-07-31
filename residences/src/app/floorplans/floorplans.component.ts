ad5c7e089063f5e88df9edc2648ee392c4c19eba
regina-bosibori <msomegina@gmail.com>
2024-08-01 00:28:53 +0300

    added floor plan component and image

---
+++ residences/src/app/floorplans/floorplans.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-floorplans',
+  standalone: true,
+  imports: [],
+  templateUrl: './floorplans.component.html',
+  styleUrl: './floorplans.component.css'
+})
+export class FloorplansComponent {
+
+}
