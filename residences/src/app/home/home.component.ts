c83a731cab609ccf3bc50379cd0b739d6683b2e4
regina-bosibori <msomegina@gmail.com>
2024-07-31 23:57:38 +0300

    added swiper section

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HomeIntroComponent } from '../home-intro/home-intro.component';
+import { SwiperComponent } from '../swiper/swiper.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
