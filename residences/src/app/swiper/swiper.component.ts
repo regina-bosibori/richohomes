6fb95a95a2e0bb799c5ce4b6c6ed7d659f733aa5
regina-bosibori <msomegina@gmail.com>
2024-07-31 23:58:10 +0300

    element positioning

---
+++ residences/src/app/swiper/swiper.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-swiper',
+  standalone: true,
+  imports: [],
+  templateUrl: './swiper.component.html',
+  styleUrl: './swiper.component.css'
+})
+export class SwiperComponent {
+
+}
