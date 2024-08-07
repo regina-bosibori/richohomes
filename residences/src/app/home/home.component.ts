d8f28bdec46afcf69c33ae14dc35fde7d595f052
regina-bosibori <msomegina@gmail.com>
2024-08-07 16:17:59 +0300

    floor plan component config

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HomeIntroComponent } from '../home-intro/home-intro.component';
 import { SwiperComponent } from '../swiper/swiper.component';
+import {FloorplansComponent} from '../floorplans/floorplans.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
