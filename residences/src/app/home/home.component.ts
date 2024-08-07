648b4789d2f9c621c209620fea9ab3c229d739b6
regina-bosibori <msomegina@gmail.com>
2024-08-07 17:03:34 +0300

    amenities component config

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { HomeIntroComponent } from '../home-intro/home-intro.component';
 import { SwiperComponent } from '../swiper/swiper.component';
-import {FloorplansComponent} from '../floorplans/floorplans.component'
+import {FloorplansComponent} from '../floorplans/floorplans.component';
+import {AmenitiesComponent} from '../amenities/amenities.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
