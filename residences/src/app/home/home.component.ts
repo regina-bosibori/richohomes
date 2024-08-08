160ff74d6bc5d80c8d5b2bfd57850f893e53eea5
regina-bosibori <msomegina@gmail.com>
2024-08-08 15:34:39 +0300

    footer component config

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -5,12 +5,13 @@ import { SwiperComponent } from '../swiper/swiper.component';
 import {FloorplansComponent} from '../floorplans/floorplans.component';
 import {AmenitiesComponent} from '../amenities/amenities.component';
 import {Swiper2Component} from '../swiper2/swiper2.component';
-import {MapComponent} from '../map/map.component'
+import {MapComponent} from '../map/map.component';
+import { FooterComponent } from '../footer/footer.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component,MapComponent],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component,MapComponent, FooterComponent ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
