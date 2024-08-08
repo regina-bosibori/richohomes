ae52e1a21b2aa2232fbf0023c29f59f3d51a65a2
regina-bosibori <msomegina@gmail.com>
2024-08-08 11:20:44 +0300

    swiper2 config

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -3,12 +3,13 @@ import { HeaderComponent } from '../header/header.component';
 import { HomeIntroComponent } from '../home-intro/home-intro.component';
 import { SwiperComponent } from '../swiper/swiper.component';
 import {FloorplansComponent} from '../floorplans/floorplans.component';
-import {AmenitiesComponent} from '../amenities/amenities.component'
+import {AmenitiesComponent} from '../amenities/amenities.component';
+import {Swiper2Component} from '../swiper2/swiper2.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
