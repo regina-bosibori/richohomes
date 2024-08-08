111698138e5b60ce75a668467abae92fa75c965a
regina-bosibori <msomegina@gmail.com>
2024-08-08 14:04:29 +0300

    map component config

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -4,12 +4,13 @@ import { HomeIntroComponent } from '../home-intro/home-intro.component';
 import { SwiperComponent } from '../swiper/swiper.component';
 import {FloorplansComponent} from '../floorplans/floorplans.component';
 import {AmenitiesComponent} from '../amenities/amenities.component';
-import {Swiper2Component} from '../swiper2/swiper2.component'
+import {Swiper2Component} from '../swiper2/swiper2.component';
+import {MapComponent} from '../map/map.component'
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component,MapComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
