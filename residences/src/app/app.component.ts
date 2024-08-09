132a8f706478d6c578e22c77044a80d5af90031e
regina-bosibori <msomegina@gmail.com>
2024-08-09 15:35:59 +0300

    imported components to root

--- residences/src/app/app.component.ts
+++ residences/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {SwiperComponent} from './swiper/swiper.component';
+import { AmenitiesComponent} from './amenities/amenities.component';
+import { FloorplansComponent } from './floorplans/floorplans.component';
+import { RouterModule } from '@angular/router';
+import { MapComponent } from './map/map.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,HomeComponent],
+  imports: [RouterOutlet,RouterModule,HomeComponent,SwiperComponent,AmenitiesComponent,FloorplansComponent,MapComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
