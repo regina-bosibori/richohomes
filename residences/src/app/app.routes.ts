a9e7ebaa16dbee3ff571f9f15331bc89764f3acd
regina-bosibori <msomegina@gmail.com>
2024-08-09 15:35:01 +0300

    imported components for routing

--- residences/src/app/app.routes.ts
+++ residences/src/app/app.routes.ts
@@ -1,5 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import {SwiperComponent} from './swiper/swiper.component'
+import { AmenitiesComponent } from './amenities/amenities.component';
+import { FloorplansComponent } from './floorplans/floorplans.component';
+import { MapComponent } from './map/map.component';
 
 export const routes: Routes = [
     {
@@ -9,4 +13,25 @@ export const routes: Routes = [
     {
         path: "home",
         component: HomeComponent
-    },]
+    },
+    {
+        path: "residences",
+        component: SwiperComponent
+
+        
+    },
+    {
+        path: "amenities" ,
+        component: AmenitiesComponent
+    },
+
+    {
+        path: "location" ,
+        component: MapComponent
+    },
+    {
+        path: "floorPlans" ,
+        component:FloorplansComponent
+    }
+    
+]
