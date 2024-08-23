56c0b59b0374ea5cd71f9096abb13ff565a308ba
regina-bosibori <msomegina@gmail.com>
2024-08-23 18:10:40 +0300

    download bronchure button content

--- residences/src/app/home-intro/home-intro.component.ts
+++ residences/src/app/home-intro/home-intro.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialog, MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+import { DownloadpdfComponent } from '../downloadpdf/downloadpdf.component';
+import { DialogModule } from '@angular/cdk/dialog';
 
 @Component({
   selector: 'app-home-intro',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,DownloadpdfComponent,MatDialogModule,MatDialogActions,DialogModule],
   templateUrl: './home-intro.component.html',
   styleUrl: './home-intro.component.css'
 })
 export class HomeIntroComponent {
+  constructor(public dialog: MatDialog) {}
+  openDialogue():void{
+    const dialogRef = this.dialog.open(DownloadpdfComponent);
+  }
+
 
 }
