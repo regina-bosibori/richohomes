1deb2e5e7a6b445df56034b2ed0a9f92f7651d7f
regina-bosibori <msomegina@gmail.com>
2024-08-23 17:29:15 +0300

    Mat dialogue modules

---
+++ residences/src/app/app.module.ts
@@ -0,0 +1,49 @@
+import {MatButtonModule} from '@angular/material/button';
+import {
+  MAT_DIALOG_DATA,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+
+} from '@angular/material/dialog';
+import { MatDialogModule } from '@angular/material/dialog';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material/input';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import {MatCheckboxModule } from '@angular/material/checkbox';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    MatButtonModule, 
+    MatFormFieldModule,
+    MatInputModule,
+    MatDialogModule,
+  MatDialog,
+  MatDialogActions,
+  MatDialogClose,
+  MatDialogContent,
+  MatDialogRef,
+  MatDialogTitle,
+  FormsModule,
+  MatCheckboxModule
+
+
+
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
