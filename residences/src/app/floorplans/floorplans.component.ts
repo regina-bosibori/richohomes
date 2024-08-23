01d946916101fa41832c9fd2c8d0d33054c59523
regina-bosibori <msomegina@gmail.com>
2024-08-23 17:22:56 +0300

    download bronchure button functionality

--- residences/src/app/floorplans/floorplans.component.ts
+++ residences/src/app/floorplans/floorplans.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import { MatDialog, MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+import { MatDialogRef } from '@angular/material/dialog';
+import { DownloadpdfComponent } from '../downloadpdf/downloadpdf.component';
+import { DialogModule } from '@angular/cdk/dialog';
+
+
+
 
 @Component({
   selector: 'app-floorplans',
   standalone: true,
-  imports: [],
+  imports: [MatButtonModule,DownloadpdfComponent,MatDialogModule,MatDialogActions,DialogModule], 
+    
   templateUrl: './floorplans.component.html',
   styleUrl: './floorplans.component.css'
 })
 export class FloorplansComponent {
+  constructor(public dialog: MatDialog) {}
+  openDialogue():void{
+    const dialogRef = this.dialog.open(DownloadpdfComponent);
+  }
 
 }
