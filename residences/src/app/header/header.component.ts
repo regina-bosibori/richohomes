3bd534841fbb97a123632f639cbffea5513662a0
regina-bosibori <msomegina@gmail.com>
2024-09-04 09:52:38 +0300

    header button links

--- residences/src/app/header/header.component.ts
+++ residences/src/app/header/header.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { DownloadpdfComponent } from '../downloadpdf/downloadpdf.component';
+import { DialogModule } from '@angular/cdk/dialog';
+import { MatDialog, MatDialogActions, MatDialogModule } from '@angular/material/dialog';
+
+
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule,DownloadpdfComponent,MatDialogModule,MatDialogActions,DialogModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+  constructor(public dialog: MatDialog) {}
+  openDialogue():void{
+    const dialogRef = this.dialog.open(DownloadpdfComponent);
+  }
 
 }
