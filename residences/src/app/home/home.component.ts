8e75b323c4e6e5d8fc555ab6d885eaaf5c15645d
regina-bosibori <msomegina@gmail.com>
2024-09-04 09:51:10 +0300

    home id

--- residences/src/app/home/home.component.ts
+++ residences/src/app/home/home.component.ts
@@ -6,15 +6,16 @@ import {FloorplansComponent} from '../floorplans/floorplans.component';
 import {AmenitiesComponent} from '../amenities/amenities.component';
 import {Swiper2Component} from '../swiper2/swiper2.component';
 import {MapComponent} from '../map/map.component';
-import { FooterComponent } from '../footer/footer.component'
+import { FooterComponent } from '../footer/footer.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component,MapComponent, FooterComponent ],
+  imports: [HeaderComponent,HomeIntroComponent,SwiperComponent,FloorplansComponent,AmenitiesComponent,Swiper2Component,MapComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  
 
 }
